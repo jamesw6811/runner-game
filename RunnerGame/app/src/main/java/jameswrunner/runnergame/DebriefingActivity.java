56f93b528591f553e2fc53e4b09b2d0a338c051a
James White <jamesw6811@gmail.com>
2019-03-29 19:10:52 -0400

    Fix #27

--- RunnerGame/app/src/main/java/jameswrunner/runnergame/DebriefingActivity.java
+++ RunnerGame/app/src/main/java/jameswrunner/runnergame/DebriefingActivity.java
@@ -3,12 +3,18 @@ package jameswrunner.runnergame;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 
 public class DebriefingActivity extends Activity {
+    private static final String EMAILFEEDBACK = "jamesw6811@gmail.com";
+    private static final String[] EMAILSFEEDBACK = {EMAILFEEDBACK};
+    private static final String EMAILFEEDBACK_SUBJECT = "Super Helpful Sappy Secrets Feedbark";
+    private static final String EMAILFEEDBACK_TEMPLATE = "Sappy Secrets Developers,\n\nSappy Secrets has room to improve. I really liked:\n\n\nI didn't like:\n\n\nAnd you should also know:\n\n\n";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +30,18 @@ public class DebriefingActivity extends Activity {
     }
 
     private void finishDebrief() {
-        // TODO: Ask for feedback.
+        composeEmail(EMAILSFEEDBACK, EMAILFEEDBACK_SUBJECT, EMAILFEEDBACK_TEMPLATE);
         finish();
+    }
+
+    private void composeEmail(String[] addresses, String subject, String text) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
